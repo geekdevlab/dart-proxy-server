df379c4cc2fd20f18f00b7a73887276a1b970f52
matty <devymatty@gmail.com>
2024-05-22 22:36:03 +0300

    update

--- proxy_server/install.sh
+++ proxy_server/install.sh
@@ -20,7 +20,6 @@ SCRIPTS=(
     "$BASE_URL/proxy_server/setup_server.sh"
     "$BASE_URL/proxy_server/setup_service.sh"
     "$BASE_URL/proxy_server/setup_nginx.sh"
-    "$BASE_URL/proxy_server/setup_https.sh"
 )
 
 # Загрузка и выполнение скриптов
